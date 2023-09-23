56c6572f243ea95c27e4e66ca6f5f452369dc744
beloveyeboah <beloveyeboah@gmail.com>
2023-09-23 02:03:25 -0700

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+void _swap(int *a, int *p);
+
+
+/**
+ * _swap - swaps the index of two values
+ * @a: an array
+ * @b: an array
+ * Return: anything
+ */
+
+void _swap(int *a, int *p)
+{
+	int swap;
+
+	swap = *a;
+	*a = *p;
+	*p = swap;
+}
+
+	
+/**
+ * bubble_sort - sorts sorts an array of integers
+ * @array: the name of the array
+ * @size: the lenght of the array
+ * Return: anything
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t j, i = 0;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				_swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
