b5c718281c167001f4862cc57e6951bb2f63f226
beloveyeboah <beloveyeboah@gmail.com>
2023-09-23 13:04:36 -0700

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+void _swap(int *a, int *b);
+
+/**
+ * _swap - swaps index of an element
+ * @a: value at previous index
+ * @b: value at next index
+ * Return: the swap values
+ */
+void _swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - sorts an array using the Selection sort algorithm
+ * @size: the size of array
+ * @array: name of the array
+ * Return: sorted list
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	size_t mini;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		mini = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[mini])
+			{
+				mini = j;
+			}
+		}
+		if (mini != i)
+		{
+			_swap(&array[i], &array[mini]);
+			print_array(array, size);
+		}
+	}
+}
