b537e28241415f82eb0418ddafe805711ee74786
beloveyeboah <beloveyeboah@gmail.com>
2023-09-25 13:16:20 -0700

    bub

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,29 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t temp, i, j;
-	int flag;
+	int flag = 1;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	while (flag == 1)
 	{
 		flag = 0;
-		for (j = 0; j < size - 1 - i; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - 1 - i; j++)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				flag = 1;
-
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					flag = 1;
+					print_array(array, size);
+				}
 			}
 		}
 		if (flag == 0)
 			break;
-
 	}
 }
